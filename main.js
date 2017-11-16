07a10892d0c4f4688fb144525445636d3306f7c4
neateye <33737003+neateye@users.noreply.github.com>
2017-11-16 15:41:07 -0800

    Created math function

---
+++ main.js
@@ -0,0 +1,9 @@
+function math() {
+  var answer = prompt("enter 1+1");
+  if (answer == 2) {
+    confirm("You got it right. Your answer was " + answer);
+  }
+  else { 
+    confirm("You got it wrong");
+  }
+}
