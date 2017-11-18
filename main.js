087b7cdf4c1bc4e56f810cf0ac9d13ea2793a2f0
neateye <33737003+neateye@users.noreply.github.com>
2017-11-17 20:53:36 -0800

    changed function

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-function math() {
-  var answer = prompt("enter 1+1");
-  if (answer == 2) {
-    confirm("You got it right. Your answer was " + answer);
-  }
-  else { 
-    confirm("You got it wrong");
+function quotes() {
+  if (confirm("Do you want a quote?") == true) {
+    alert("Becoming fearless isn't the point. That's impossible. It's learning how to control your fear, and how to be free from it.");
+  } if (confirm("Do you want a joke?") == true) {
+  		alert("One a scale of one to ten, how obsessed with Harry Potter are you? About nine and three quarters.");
+  } if (confirm("Do you want a pun?") == true) {
+  		alert(" Simba was walking too slow, so I told him to Mufasa.");
+  } else {
+  		alert("Aww, okay.");
   }
 }
